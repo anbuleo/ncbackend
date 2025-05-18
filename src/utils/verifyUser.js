68b5756aa8bffac09b485ac473e3d724f33588e1
anbuleo <anbuliyon@gmail.com>
2025-05-18 02:59:18 -0700

    model1

--- src/utils/verifyUser.js
+++ src/utils/verifyUser.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { errorHandler } from "./errorHandler.js";
 
 export const verifyUser = (req, res, next) => {
-    const token =req.headers.authorization.split(" ")[1]
+    const token =req.headers?.authorization?.split(" ")[1]
 
     if(!token) return next(errorHandler(401, 'Unauthorised'))
 
